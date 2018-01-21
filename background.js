b3776a28290dbc1a8bfdb84abc9024822fa7bba9
zach holbrook <zholbroo@ucsc.edu>
2018-01-21 11:45:51 -0800

    kinda works

--- background.js
+++ background.js
@@ -1,14 +1,25 @@
 function sendTextToAPI(info) {
-  axios({
-  	method: 'post',
-  	url: 'http://localhost:4567/sarc',
-  	data: {
-  		text: info.selectiontext
-  	}
-  }).then(function (response) {
-  	console.log(response)
-  }).catch(function (error) {
-  	console.log(error)
+  // axios({
+  // 	method: 'post',
+  // 	url: 'http://localhost:4567/sarc',
+  // 	data: {
+  // 		'text': info.selectiontext
+  // 	}
+  // }).then(function (response) {
+  // 	console.log(response)
+  // }).catch(function (error) {
+  // 	console.log(error)
+  // });
+  fetch('http://localhost:4567/sarc', {
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({'text': 'testtext'})
+  }).then(function(res) {
+    alert(JSON.stringify(res.body))
+  }).catch(function(error) {
+    alert(error)
   });
 }
 
