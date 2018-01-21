0a8171dcc38cb47fc1b60c3b5d2761acd07ff864
zach holbrook <zholbroo@ucsc.edu>
2018-01-21 02:30:19 -0800

    bug fixes

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
-function sendTextToAPI(selection) {
-  axios.post('http://localhost:4567', {
+function sendTextToAPI(info) {
+  axios.post('http://localhost:4567/sarc', {
         text: info.selectiontext
     })
     .then(function (response) {
-      alert(response)
+      
+      console.log(response)
     }).catch(function (error) {
-      console.log(error))};  
-}
+      console.log(error)
+    }); 
+};
+
+chrome.contextMenus.create({title: "Check the selection for sarcasm", contexts:["selection"], onclick: sendTextToAPI});
+
