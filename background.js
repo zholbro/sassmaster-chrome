baa05f125b7e5278fb3dd1ece9a0e3800edde82b
zach holbrook <zholbroo@ucsc.edu>
2018-01-21 03:00:10 -0800

    remix axios post req, still not working (sassmaster returns 500's)

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 function sendTextToAPI(info) {
-  axios.post('http://localhost:4567/sarc', {
-        text: info.selectiontext
-    })
-    .then(function (response) {
-      
-      console.log(response)
-    }).catch(function (error) {
-      console.log(error)
-    }); 
-};
+  axios({
+  	method: 'post',
+  	url: 'http://localhost:4567/sarc',
+  	data: {
+  		text: info.selectiontext
+  	}
+  }).then(function (response) {
+  	console.log(response)
+  }).catch(function (error) {
+  	console.log(error)
+  });
+}
 
-chrome.contextMenus.create({title: "Check the selection for sarcasm", contexts:["selection"], onclick: sendTextToAPI});
+chrome.contextMenus.create({title: "Check this selection for sarcasm", contexts:["selection"], onclick: sendTextToAPI});
 
