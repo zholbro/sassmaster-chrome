e900026fa924756ab3198dcbd3a0bca4818f4a95
zach holbrook <zholbroo@ucsc.edu>
2018-01-21 22:28:42 -0800

    works, readme update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sassmaster-chrome
-a port of LotSX's sassmaster-firefox extension implementation
+a chrome extension that merely calls the Sassmaster API (submitted for
+consideration at cruzhacks 2018), a machine learning approach for
+sentiment analysis
+
+## use
+select text, and bring up option menu (right-click) for a new option for
+sarcasm checks
+
+## heads up
+sassmaster-chrome expects server to be running on localhost:4567/sarc, do
+make sure to change the url hard-coded into background.js
--- background.js
+++ background.js
@@ -1,32 +1,25 @@
-function sendTextToAPI(window) {
-  // axios({
-  // 	method: 'post',
-  // 	url: 'http://localhost:4567/sarc',
-  // 	data: {
-  // 		'text': info.selectiontext
-  // 	}
-  // }).then(function (response) {
-  // 	console.log(response)
-  // }).catch(function (error) {
-  // 	console.log(error)
-  // });
+function sendTextToAPI(info) {
+
   fetch('http://localhost:4567/sarc', {
       headers: {
           'Content-Type': 'application/json'
       },
       method: 'POST',
-      body: JSON.stringify({'text': window.getSelection})
+      body: JSON.stringify(
+        {'text': info.selectionText }
+      )
   }).then(function(res) {
     res.text().then(response => {
       x = JSON.parse(response)
-      //alert(JSON.stringify(x));
-      alert(`That was: ${x.prediction}. Bayes predicts: ${x.bayes.prediction}. ML predicts: ${x.ml.prediction}.`);
-      // Bayes predicts: ${x.bayes.prediction}. ML predicts: ${x.ml.prediction}.
+      alert(`That was: ${x.prediction}.
+            Bayes predicts: ${x.data.bayes.prediction}.
+            ML predicts: ${x.data.ml.prediction}.`);
     });
   }).catch(function(error) {
     alert(error)
   });
 }
 
-chrome.contextMenus.create({title: "Check this selection for sarcasm", contexts:["selection"], onclick: sendTextToAPI});
+chrome.contextMenus.create({title: "Check this selection for sarcasm", contexts:["selection"],
+                            onclick: sendTextToAPI});
 
